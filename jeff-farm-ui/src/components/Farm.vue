5e6924ff251be990be1e583e5c979bacc49a878b
ants280 <jacob.patterson@gmail.com>
2019-02-10 16:34:16 -0800

    adding farm request

---
+++ jeff-farm-ui/src/components/Farm.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <p>ittemList:</p>
+    <p>{{ itemList }}</p>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Location</th>
+      </tr>
+      <tr v-for="item in itemList" :key="item.id">
+        <td>{{ item.name }}</td>
+        <td>{{ item.location }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Farm",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      itemList: []
+    };
+  },
+  methods: {
+    getTable() {
+      axios.get("farm").then(response => {
+        console.log(response);
+        this.itemList = response;
+      });
+    }
+  },
+  mounted: function() {
+    this.getTable();
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
--- jeff-farm-ui/src/main.js
+++ jeff-farm-ui/src/main.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
 
 Vue.config.productionTip = false;
 
+axios.defaults.headers.common["Access-Control-Allow-Origin"] =
+  "http://localhost:8081";
+axios.defaults.headers.common["Access-Control-Allow-Headers"] =
+  "Origin, X-Requested-With, Content-Type, Accept";
+
+axios.defaults.baseURL = "http://localhost:8080/jeff-farm-ws/";
 new Vue({
   router,
   render: h => h(App)
