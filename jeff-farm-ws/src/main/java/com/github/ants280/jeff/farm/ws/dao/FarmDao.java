24acdd8ea65d3952dd23492c8ac18d7e8ca495c0
ants280 <jacob.patterson@gmail.com>
2019-02-14 11:40:29 -0800

    Using StoredProcedureDao.java for spring interaction.

--- jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/dao/FarmDao.java
+++ jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/dao/FarmDao.java
@@ -2,97 +2,59 @@ package com.github.ants280.jeff.farm.ws.dao;
 
 import com.github.ants280.jeff.farm.ws.model.Farm;
 import java.sql.Types;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import javax.inject.Inject;
 import javax.sql.DataSource;
 import org.jvnet.hk2.annotations.Service;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.SqlOutParameter;
-import org.springframework.jdbc.core.SqlParameter;
-import org.springframework.jdbc.core.SqlReturnUpdateCount;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 
 @Service
-public class FarmDao implements CrudDao<Farm>
+public class FarmDao extends StoredProcedureDao implements CrudDao<Farm>
 {
-	private final JdbcTemplate jdbcTemplate;
-	// TODO: Move this to BaseDao (and make abstract)
-	private static final String RETURN_UPDATE_COUNT = "UPDATE_COUNT";
-	private static final SqlParameter RETURN_UPDATE_COUNT_SQL_PARAMETER
-			= new SqlReturnUpdateCount(RETURN_UPDATE_COUNT);
-
 	@Inject
 	public FarmDao(DataSource dataSource)
 	{
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		super(dataSource);
 	}
 
 	@Override
 	public int create(Farm farm)
 	{
-		SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
-				.addValue("farmName", farm.getName())
-				.addValue("farmLocation", farm.getLocation());
-
-		SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
-				.withProcedureName("createFarm")
-				.declareParameters(new SqlParameter("farmName", Types.VARCHAR))
-				.declareParameters(new SqlParameter("farmLocation", Types.VARCHAR))
-				.declareParameters(new SqlOutParameter("farmID", Types.INTEGER));
-
-		Map<String, Object> outputParams = simpleJdbcCall.execute(sqlParameterSource);
-		assert (int) outputParams.get(RETURN_UPDATE_COUNT) == 1;
-		return (int) outputParams.get("farmID");
+		return this.executeCreate(
+				"createFarm",
+				Arrays.asList(
+						new Parameter("farmName", farm.getName(), Types.VARCHAR),
+						new Parameter("farmLocation", farm.getLocation(), Types.VARCHAR)),
+				"farmID");
 	}
 
 	@Override
 	public List<Farm> read()
 	{
-		SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
-				.withProcedureName("readFarms")
-				.returningResultSet("resultSet", new Farm.ResultSetExtractor())
-				.declareParameters(RETURN_UPDATE_COUNT_SQL_PARAMETER);
-
-		Map<String, Object> outputParams = simpleJdbcCall.execute();
-		assert (int) outputParams.get(RETURN_UPDATE_COUNT) == 0;
-		return (List<Farm>) outputParams.get("resultSet");
+		return this.executeRead(
+				"readFarms",
+				Collections.emptyList(),
+				new Farm.ResultSetExtractor());
 	}
 
 	@Override
 	public void update(Farm farm)
 	{
-		SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
-				.addValue("farmID", farm.getId())
-				.addValue("farmName", farm.getName())
-				.addValue("farmLocation", farm.getLocation());
-
-		SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
-				.withProcedureName("updateFarm")
-				.declareParameters(new SqlParameter("farmID", Types.INTEGER))
-				.declareParameters(new SqlParameter("farmName", Types.VARCHAR))
-				.declareParameters(new SqlParameter("farmLocation", Types.VARCHAR))
-				.declareParameters(RETURN_UPDATE_COUNT_SQL_PARAMETER);
-
-		Map<String, Object> outputParams
-				= simpleJdbcCall.execute(sqlParameterSource);
-		assert (int) outputParams.get(RETURN_UPDATE_COUNT) == 1;
+		this.executeUpdate(
+				"updateFarm",
+				Arrays.asList(
+						new Parameter("farmID", farm.getId(), Types.INTEGER),
+						new Parameter("farmName", farm.getName(), Types.VARCHAR),
+						new Parameter("farmLocation", farm.getLocation(), Types.VARCHAR)));
 	}
 
 	@Override
 	public void delete(int id)
 	{
-		SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
-				.addValue("farmID", id);
-
-		SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
-				.withProcedureName("deleteFarm")
-				.declareParameters(new SqlParameter("farmID", Types.INTEGER))
-				.declareParameters(RETURN_UPDATE_COUNT_SQL_PARAMETER);
-
-		Map<String, Object> outputParams = simpleJdbcCall.execute(sqlParameterSource);
-		assert (int) outputParams.get(RETURN_UPDATE_COUNT) == 1;
+		this.executeUpdate(
+				"deleteFarm",
+				Collections.singletonList(
+						new Parameter("farmID", id, Types.INTEGER)));
 	}
 }
---
+++ jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/dao/StoredProcedureDao.java
@@ -0,0 +1,141 @@
+package com.github.ants280.jeff.farm.ws.dao;
+
+import java.sql.Types;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SqlOutParameter;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.SqlReturnUpdateCount;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+
+public class StoredProcedureDao
+{
+	private final JdbcTemplate jdbcTemplate;
+
+	private static final String STORED_PROCEDURE_NAME = "storedProcedureName";
+	private static final String RETURN_UPDATE_COUNT = "UPDATE_COUNT";
+	private static final SqlParameter RETURN_UPDATE_COUNT_SQL_PARAMETER
+			= new SqlReturnUpdateCount(RETURN_UPDATE_COUNT);
+
+	public StoredProcedureDao(DataSource dataSource)
+	{
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public void executeUpdate(
+			String storedProcedureName,
+			Collection<Parameter> inParameters)
+	{
+		Map<String, Object> outputParams = this.execute(
+				inParameters,
+				storedProcedureName,
+				null,
+				null);
+
+		assert (int) outputParams.get(RETURN_UPDATE_COUNT) == 1;
+	}
+
+	public int executeCreate(
+			String storedProcedureName,
+			Collection<Parameter> inParameters,
+			String outParameterName)
+	{
+		Map<String, Object> outputParams = this.execute(
+				inParameters,
+				storedProcedureName,
+				outParameterName,
+				null);
+
+		assert (int) outputParams.get(RETURN_UPDATE_COUNT) == 1;
+
+		return (int) outputParams.get(outParameterName);
+	}
+
+	public <T> List<T> executeRead(
+			String storedProcedureName,
+			Collection<Parameter> inParameters,
+			RowMapper<T> rowMapper)
+	{
+		Map<String, Object> outputParams = this.execute(
+				inParameters,
+				storedProcedureName,
+				null,
+				rowMapper);
+
+		assert (int) outputParams.get(RETURN_UPDATE_COUNT) == 0;
+
+		return (List<T>) outputParams.get(STORED_PROCEDURE_NAME);
+	}
+
+	private <T> Map<String, Object> execute(
+			Collection<Parameter> inParameters,
+			String storedProcedureName,
+			String outParameterName,
+			RowMapper<T> rowMapper)
+	{
+		MapSqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+				.addValues(inParameters.stream()
+						.collect(Collectors.toMap(
+								Parameter::getName,
+								Parameter::getValue)));
+		SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
+				.withProcedureName(storedProcedureName)
+				.declareParameters(inParameters.stream()
+						.map(parameter -> new SqlParameter(
+						parameter.getName(),
+						parameter.getSqlType()))
+						.toArray(SqlParameter[]::new))
+				.declareParameters(RETURN_UPDATE_COUNT_SQL_PARAMETER);
+		
+		assert outParameterName == null ^ rowMapper == null;
+		if (outParameterName != null)
+		{
+			simpleJdbcCall.declareParameters(new SqlOutParameter(
+					outParameterName,
+					Types.INTEGER));
+		}
+		if (rowMapper != null)
+		{
+			simpleJdbcCall.returningResultSet(
+					STORED_PROCEDURE_NAME,
+					rowMapper);
+		}
+
+		return simpleJdbcCall.execute(sqlParameterSource);
+	}
+
+	public static class Parameter<T>
+	{
+		private final String name;
+		private final T value;
+		private final int sqlType;
+
+		public Parameter(String name, T value, int sqlType)
+		{
+			this.name = name;
+			this.value = value;
+			this.sqlType = sqlType;
+		}
+
+		public String getName()
+		{
+			return name;
+		}
+
+		public T getValue()
+		{
+			return value;
+		}
+
+		public int getSqlType()
+		{
+			return sqlType;
+		}
+	}
+}
