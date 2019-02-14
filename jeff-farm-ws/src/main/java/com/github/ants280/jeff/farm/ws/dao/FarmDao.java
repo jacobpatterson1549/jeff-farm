88b4457a0354339982a20a0bbc2f592505ab838c
ants280 <jacob.patterson@gmail.com>
2019-02-13 17:07:42 -0800

    checking status with SqlReturnUpdateCount SqlParameter

--- jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/dao/FarmDao.java
+++ jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/dao/FarmDao.java
@@ -3,12 +3,14 @@ package com.github.ants280.jeff.farm.ws.dao;
 import com.github.ants280.jeff.farm.ws.model.Farm;
 import java.sql.Types;
 import java.util.List;
+import java.util.Map;
 import javax.inject.Inject;
 import javax.sql.DataSource;
 import org.jvnet.hk2.annotations.Service;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SqlOutParameter;
 import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.SqlReturnUpdateCount;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
@@ -17,6 +19,10 @@ import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 public class FarmDao implements BaseDao<Farm>
 {
 	private final JdbcTemplate jdbcTemplate;
+	// TODO: Move this to BaseDao (and make abstract)
+	private static final String RETURN_UPDATE_COUNT = "UPDATE_COUNT";
+	private static final SqlParameter RETURN_UPDATE_COUNT_SQL_PARAMETER
+			= new SqlReturnUpdateCount(RETURN_UPDATE_COUNT);
 
 	@Inject
 	public FarmDao(DataSource dataSource)
@@ -30,23 +36,29 @@ public class FarmDao implements BaseDao<Farm>
 		SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
 				.addValue("farmName", farm.getName())
 				.addValue("farmLocation", farm.getLocation());
-		
+
 		SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
 				.withProcedureName("createFarm")
 				.declareParameters(new SqlParameter("farmName", Types.VARCHAR))
 				.declareParameters(new SqlParameter("farmLocation", Types.VARCHAR))
 				.declareParameters(new SqlOutParameter("farmID", Types.INTEGER));
-		
-		return (int) simpleJdbcCall.execute(sqlParameterSource)
-				.get("farmID");
+
+		Map<String, Object> outputParams = simpleJdbcCall.execute(sqlParameterSource);
+		assert (int) outputParams.get(RETURN_UPDATE_COUNT) == 1;
+		return (int) outputParams.get("farmID");
 	}
 
 	@Override
 	public List<Farm> read()
 	{
-		return jdbcTemplate.query(
-				"call readFarms",
-				new Farm.ResultSetExtractor());
+		SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
+				.withProcedureName("readFarms")
+				.returningResultSet("resultSet", new Farm.ResultSetExtractor())
+				.declareParameters(RETURN_UPDATE_COUNT_SQL_PARAMETER);
+
+		Map<String, Object> outputParams = simpleJdbcCall.execute();
+		assert (int) outputParams.get(RETURN_UPDATE_COUNT) == 0;
+		return (List<Farm>) outputParams.get("resultSet");
 	}
 
 	@Override
@@ -61,9 +73,12 @@ public class FarmDao implements BaseDao<Farm>
 				.withProcedureName("updateFarm")
 				.declareParameters(new SqlParameter("farmID", Types.INTEGER))
 				.declareParameters(new SqlParameter("farmName", Types.VARCHAR))
-				.declareParameters(new SqlParameter("farmLocation", Types.VARCHAR));
-		
-		simpleJdbcCall.execute(sqlParameterSource);
+				.declareParameters(new SqlParameter("farmLocation", Types.VARCHAR))
+				.declareParameters(RETURN_UPDATE_COUNT_SQL_PARAMETER);
+
+		Map<String, Object> outputParams
+				= simpleJdbcCall.execute(sqlParameterSource);
+		assert (int) outputParams.get(RETURN_UPDATE_COUNT) == 1;
 	}
 
 	@Override
@@ -74,8 +89,10 @@ public class FarmDao implements BaseDao<Farm>
 
 		SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
 				.withProcedureName("deleteFarm")
-				.declareParameters(new SqlParameter("farmID", Types.INTEGER));
-		
-		simpleJdbcCall.execute(sqlParameterSource);
+				.declareParameters(new SqlParameter("farmID", Types.INTEGER))
+				.declareParameters(RETURN_UPDATE_COUNT_SQL_PARAMETER);
+
+		Map<String, Object> outputParams = simpleJdbcCall.execute(sqlParameterSource);
+		assert (int) outputParams.get(RETURN_UPDATE_COUNT) == 1;
 	}
 }
