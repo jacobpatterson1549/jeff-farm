573a45e139b175c1166a5a851d5aa66de428f2da
ants280 <jacob.patterson@gmail.com>
2019-02-14 11:46:17 -0800

    using normal way of creating HashSet.

--- jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/ApplicationConfig.java
+++ jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package com.github.ants280.jeff.farm.ws;
 
 import com.github.ants280.jeff.farm.ws.resources.*;
+import java.util.HashSet;
 import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -11,7 +12,7 @@ public class ApplicationConfig extends Application
 	@Override 
 	public Set<Class<?>> getClasses()
 	{
-		Set<Class<?>> resources = new java.util.HashSet<>();
+		Set<Class<?>> resources = new HashSet<>();
 		resources.add(FarmResource.class);
 		return resources;
 	}
@@ -19,7 +20,7 @@ public class ApplicationConfig extends Application
 	@Override
 	public Set<Object> getSingletons()
 	{
-		Set<Object> singletons = new java.util.HashSet<>();
+		Set<Object> singletons = new HashSet<>();
 		singletons.add(new InjectionBinder());
 		return singletons;
 	}
