fcd961e6532484ea8e3e40c9009e0e2d08dc6e8a
ants280 <jacob.patterson@gmail.com>
2019-02-21 11:00:55 -0800

    setting the ui server to port 4200 (ngular)

--- jeff-farm-ui/src/app/farm.service.ts
+++ jeff-farm-ui/src/app/farm.service.ts
@@ -7,10 +7,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { Farm } from './classes/farm';
 
 const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://localhost:4200'
-  })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
 @Injectable({
