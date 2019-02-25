5d90607500f2f16a46169512a8a71f89cdffbf40
Jacob Patterson <jacob.patterson@gmail.com>
2019-02-24 17:12:55 -0800

    removing unused code

--- jeff-farm-ui/src/app/services/farm.service.ts
+++ jeff-farm-ui/src/app/services/farm.service.ts
@@ -1,15 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
-
 import { Farm } from '../classes/farm';
 import { CrudService } from './crud.service';
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
 
 @Injectable({
   providedIn: 'root'
