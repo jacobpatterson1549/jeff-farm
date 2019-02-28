c380249f8c99fc5368018b53316d4130f88bfed9
Jacob Patterson <jacob.patterson@gmail.com>
2019-02-28 11:28:40 -0800

    removing unused OnInit implementations, constructors from components

--- jeff-farm-ui/src/app/components/farms.component.ts
+++ jeff-farm-ui/src/app/components/farms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { FarmService } from '../services/farm.service';
 import { FormType } from '../classes/form.type';
@@ -8,52 +8,44 @@ import { Farm } from '../classes/farm';
   selector: 'app-list-farms',
   template: '<app-crud-list [crudService]="farmService" [displayField]="displayField"></app-crud-list>'
 })
-export class FarmsListComponent implements OnInit {
+export class FarmsListComponent {
 
   displayField: string = 'name';
   
   constructor(private farmService: FarmService) { }
-
-  ngOnInit() { }
 }
 
 @Component({
   selector: 'app-farms-view',
   template: '<app-crud-view [crudService]="farmService" [itemNames]="itemNames"></app-crud-view>'
 })
-export class FarmsViewComponent implements OnInit {
+export class FarmsViewComponent {
 
   itemNames: string[] = Farm.ITEM_NAMES;
   
   constructor(private farmService: FarmService) { }
-
-  ngOnInit() { }
 }
 
 @Component({
   selector: 'app-farms-create',
   template: '<app-crud-form [crudService]="farmService" [formType]="formType" [crudItem]="crudItem"></app-crud-form>'
 })
-export class FarmsCreateComponent implements OnInit {
+export class FarmsCreateComponent {
 
   formType = FormType.Create;
   crudItem: Farm = new Farm();
   
   constructor(private farmService: FarmService) { }
-
-  ngOnInit() { }
 }
 
 @Component({
   selector: 'app-farms-update',
   template: '<app-crud-form [crudService]="farmService" [formType]="formType" [crudItem]="crudItem"></app-crud-form>'
 })
-export class FarmsUpdateComponent implements OnInit {
+export class FarmsUpdateComponent {
 
   formType = FormType.Update;
   crudItem: Farm = new Farm();
     
   constructor(private farmService: FarmService) { }
-
-  ngOnInit() { }
 }
--- jeff-farm-ui/src/app/components/page-not-found.component.ts
+++ jeff-farm-ui/src/app/components/page-not-found.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   template: '<h2>Page not found</h2>',
   styles: ['h2 { color: red; }']
 })
-export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() { }
-}
+export class PageNotFoundComponent { }
