f298cf82956ad6e1ebf84bd3ed583919c4cfec11
Jacob Patterson <jacob.patterson@gmail.com>
2019-02-27 17:41:00 -0800

    removing currently unused/unverified code

--- jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/ApplicationConfig.java
+++ jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/ApplicationConfig.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import org.glassfish.jersey.jackson.JacksonFeature;
 
 @ApplicationPath("/resources")
 public class ApplicationConfig extends Application
@@ -14,7 +13,6 @@ public class ApplicationConfig extends Application
 	public Set<Class<?>> getClasses()
 	{
 		Set<Class<?>> resources = new HashSet<>();
-		resources.add(JacksonFeature.class);
 		resources.add(RootResource.class);
 		resources.add(FarmResource.class);
 		resources.add(HiveResource.class);
