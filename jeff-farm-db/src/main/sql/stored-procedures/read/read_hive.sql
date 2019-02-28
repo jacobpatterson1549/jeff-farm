3ac24c37ec924f26660290e0ac9135b6748839c5
Jacob Patterson <jacob.patterson@gmail.com>
2019-02-28 15:25:40 -0800

    adding farm_id to returned columns

--- jeff-farm-db/src/main/sql/stored-procedures/read/read_hive.sql
+++ jeff-farm-db/src/main/sql/stored-procedures/read/read_hive.sql
@@ -6,7 +6,7 @@ CREATE PROCEDURE read_hive (
 	IN id INT)
 
 	BEGIN
-		SELECT h.id, h.name, h.created_date, h.modified_date
+		SELECT h.id, h.farm_id, h.name, h.created_date, h.modified_date
 		FROM hives AS h
 		WHERE h.id = id
 			AND h.active = 1;
