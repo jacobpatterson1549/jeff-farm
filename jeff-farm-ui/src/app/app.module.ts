9130a8bd287d6c2831987955195281173d1b71a1
Jacob Patterson <jacob.patterson@gmail.com>
2019-03-02 10:34:13 -0800

    moving roture module into app module to reduce imports

--- jeff-farm-ui/src/app/app.module.ts
+++ jeff-farm-ui/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule }   from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './components/page-not-found.component';
 import { CrudListComponent } from './components/crud-list/crud-list.component';
@@ -12,6 +12,26 @@ import { CrudFormComponent } from './components/crud-form/crud-form.component';
 import { FarmsListComponent, FarmsViewComponent, FarmsCreateComponent, FarmsUpdateComponent } from './components/farms.component';
 import { HivesListComponent, HivesViewComponent, HivesCreateComponent, HivesUpdateComponent } from './components/hives.component';
 import { HiveInspectionsListComponent, HiveInspectionsViewComponent, HiveInspectionsCreateComponent, HiveInspectionsUpdateComponent } from './components/hive-inspections.component';
+
+const routes: Routes = [
+  { path: 'farms', component: FarmsListComponent },
+  { path: 'farms/create', component: FarmsCreateComponent },
+  { path: 'farms/:id', component: FarmsViewComponent },
+  { path: 'farms/:id/update', component: FarmsUpdateComponent },
+
+  { path: 'farms/:farm_id/hives', component: HivesListComponent },
+  { path: 'farms/:farm_id/hives/create', component: HivesCreateComponent },
+  { path: 'farms/:farm_id/hives/:id', component: HivesViewComponent },
+  { path: 'farms/:farm_id/hives/:id/update', component: HivesUpdateComponent },
+
+  { path: 'farms/:farm_id/hives/:hive_id/hiveInspections', component: HiveInspectionsListComponent },
+  { path: 'farms/:farm_id/hives/:hive_id/hiveInspections/create', component: HiveInspectionsCreateComponent },
+  { path: 'farms/:farm_id/hives/:hive_id/hiveInspections/:id', component: HiveInspectionsViewComponent },
+  { path: 'farms/:farm_id/hives/:hive_id/hiveInspections/:id/update', component: HiveInspectionsUpdateComponent },
+
+  { path: '', redirectTo: '/farms', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -28,7 +48,7 @@ import { HiveInspectionsListComponent, HiveInspectionsViewComponent, HiveInspect
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule,
+    RouterModule.forRoot(routes, { enableTracing: false /* true for debugging purposes only */ })
   ],
   providers: [],
   bootstrap: [AppComponent]
