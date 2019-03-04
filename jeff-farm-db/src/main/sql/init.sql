22bbbbf6b428047c86d93c48ef2adc6b99c2cdd8
Jacob Patterson <jacob.patterson@gmail.com>
2019-03-04 09:18:44 -0800

    adding comment

--- jeff-farm-db/src/main/sql/init.sql
+++ jeff-farm-db/src/main/sql/init.sql
@@ -4,7 +4,7 @@ CREATE USER 'fred'@'localhost' IDENTIFIED BY 'flintstone';
 GRANT ALL PRIVILEGES ON JEFF_FARM_DB.* TO 'fred'@'localhost' WITH GRANT OPTION;
 -- exit
 
--- !!! Connector-J !!! --
--- wget https:$$dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java_8.0.15-1ubuntu18.04_all.deb
+-- !!! Connector-J !!! --  Should only be needed to run code in ide.  Default Tomcat libs should suffice for jdbc interaction.
+-- wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java_8.0.15-1ubuntu18.04_all.deb
 -- sudo dpkg -i mysql-connector-java_8.0.15-1ubuntu18.04_all.deb || sudo -k
 -- ./asadmin add-library /usr/share/java/mysql-connector-java-8.0.15.jar
