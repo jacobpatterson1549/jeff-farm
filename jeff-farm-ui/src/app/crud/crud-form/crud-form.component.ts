23238aea6d5413f3125f0da4c24f2d5899d87d2f
Jacob Patterson <jacob.patterson@gmail.com>
2019-03-06 15:03:29 -0800

    simplifying ngOnInit()

--- jeff-farm-ui/src/app/crud/crud-form/crud-form.component.ts
+++ jeff-farm-ui/src/app/crud/crud-form/crud-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 import { CrudService } from '../crud.service';
 import { NavigationService } from '../../navigation.service';
@@ -26,39 +27,40 @@ export class CrudFormComponent<T extends CrudItem> implements OnInit {
     private navigationService: NavigationService) { }
 
   ngOnInit() {
+    this.initFormType();
+    this.submitValue = (this.formType === FormType.Update) ? 'Update' : 'Submit';
+    this.initCrudItem()
+      .subscribe(_ => this.formItems = this.crudItem.getFormItems());
+  }
 
+  private initFormType() {
     const urlParts: string[] = this.navigationService.getUrl().split('/');
     const endPath: string = urlParts[urlParts.length - 1];
     switch (endPath) {
-      case 'create' :
+      case 'create':
         this.formType = FormType.Create;
         break;
-      case 'update' :
+      case 'update':
         this.formType = FormType.Update;
         break;
       default:
         throw new Error(`Unknown endPath: ${endPath}.  Could not set FormType`);
     }
+  }
 
-    this.submitValue = (this.formType === FormType.Update) ? 'Update' : 'Submit';
-
+  private initCrudItem(): Observable<any> {
     if (this.formType == FormType.Create) {
-      this.formItems = this.crudItem.getFormItems();
-      this.crudItem = this.crudService.createCrudItem();
+      return of(this.crudItem = this.crudService.createCrudItem());
     }
     if (this.formType == FormType.Update) {
-      this.crudService.get()
-        .subscribe((data: CrudItem) => {
-          this.crudItem = Object.assign(this.crudItem, data)
-          this.formItems = this.crudItem.getFormItems();
-        });
+      return this.crudService.get()
+        .pipe(
+          tap((data: CrudItem) => this.crudItem = Object.assign(this.crudItem, data))
+        );
     }
-
-    this.formItems = this.crudItem.getFormItems();
   }
 
   submitForm() {
-
     for (let formItem of this.formItems) {
       this.crudItem[formItem.name] = formItem.value;
     }
