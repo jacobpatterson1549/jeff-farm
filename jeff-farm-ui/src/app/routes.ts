1432931dffdb1e558fc3a718c2eab6fa78e63b96
Jacob Patterson <jacob.patterson@gmail.com>
2019-03-06 13:31:57 -0800

    fixing route paths relative to RouterModule location

--- jeff-farm-ui/src/app/routes.ts
+++ jeff-farm-ui/src/app/routes.ts
@@ -1,21 +1,21 @@
 export const FARMS_ROUTES = [
     {
         path: 'farms',
-        loadChildren: './crud/crud.module#CrudModule',
+        loadChildren: '../crud/crud.module#CrudModule',
         pathMatch: 'prefix',
     }
 ];
 export const HIVES_ROUTES = [
     {
         path: 'farms/:farm_id/hives',
-        loadChildren: './crud/crud.module#CrudModule',
+        loadChildren: '../crud/crud.module#CrudModule',
         pathMatch: 'prefix',
     }
 ];
 export const HIVE_INSPECTIONS_ROUTES = [
     {
         path: 'farms/:farm_id/hives/:hive_id/hiveInspections',
-        loadChildren: './crud/crud.module#CrudModule',
+        loadChildren: '../crud/crud.module#CrudModule',
         pathMatch: 'prefix',
     }
 ];
