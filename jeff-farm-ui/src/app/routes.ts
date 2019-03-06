075166dcfad98a18f28c7bc74470a5167245952c
Jacob Patterson <jacob.patterson@gmail.com>
2019-03-06 15:57:36 -0800

    using function to provide the crud module

--- jeff-farm-ui/src/app/routes.ts
+++ jeff-farm-ui/src/app/routes.ts
@@ -3,21 +3,25 @@ import { CrudModule } from './crud/crud.module';
 export const FARMS_ROUTES = [
     {
         path: 'farms',
-        loadChildren: CrudModule,
+        loadChildren: crudEntryPoint,
         pathMatch: 'prefix',
     }
 ];
 export const HIVES_ROUTES = [
     {
         path: 'farms/:farm_id/hives',
-        loadChildren: CrudModule,
+        loadChildren: crudEntryPoint,
         pathMatch: 'prefix',
     }
 ];
 export const HIVE_INSPECTIONS_ROUTES = [
     {
         path: 'farms/:farm_id/hives/:hive_id/hiveInspections',
-        loadChildren: CrudModule,
+        loadChildren: crudEntryPoint,
         pathMatch: 'prefix',
     }
 ];
+
+function crudEntryPoint() {
+    return CrudModule;
+}
