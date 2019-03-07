17886dd8d13a63f4059242515b93720fd25c93b6
Jacob Patterson <jacob.patterson@gmail.com>
2019-03-06 16:00:15 -0800

    reverting back to path-based loadChildren argument

--- jeff-farm-ui/src/app/routes.ts
+++ jeff-farm-ui/src/app/routes.ts
@@ -3,25 +3,21 @@ import { CrudModule } from './crud/crud.module';
 export const FARMS_ROUTES = [
     {
         path: 'farms',
-        loadChildren: crudEntryPoint,
+        loadChildren: '../crud/crud.module#CrudModule',
         pathMatch: 'prefix',
     }
 ];
 export const HIVES_ROUTES = [
     {
         path: 'farms/:farm_id/hives',
-        loadChildren: crudEntryPoint,
+        loadChildren: '../crud/crud.module#CrudModule',
         pathMatch: 'prefix',
     }
 ];
 export const HIVE_INSPECTIONS_ROUTES = [
     {
         path: 'farms/:farm_id/hives/:hive_id/hiveInspections',
-        loadChildren: crudEntryPoint,
+        loadChildren: '../crud/crud.module#CrudModule',
         pathMatch: 'prefix',
     }
 ];
-
-function crudEntryPoint() {
-    return CrudModule;
-}
