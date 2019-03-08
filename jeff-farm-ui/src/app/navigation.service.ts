12fd9698b1b1a86fb5a16a839bf91cd938a38418
Jacob Patterson <jacob.patterson@gmail.com>
2019-03-08 12:23:45 -0800

    cleaning up

--- jeff-farm-ui/src/app/navigation.service.ts
+++ jeff-farm-ui/src/app/navigation.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router, ActivatedRoute, UrlSegment } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,26 @@ export class NavigationService {
   }
 
   getRouteParam(paramName: string) : number {
+    // const parentParam: string = this.activatedRoute.parent.snapshot.paramMap.get(paramName);
+    // this.activatedRoute.paramMap.pipe(
+    //   tap(params =>  {
+    //     console.log(params);
+    //     console.log('done2');
+    //   })
+    // );
+    this.activatedRoute.params.subscribe(params => {
+      // console.log(params.get(paramName));
+      console.log(params[paramName]);
+      console.log('done');
+    });
     const param: string = this.activatedRoute.snapshot.paramMap.get(paramName);
+    // const param: string = this.activatedRoute.snapshot.params[paramName];
+    // let id2 = +this.activatedRoute.snapshot.params[paramName];
+    // let params2 = +this.activatedRoute.snapshot.params;
+
+    if (param == null) {
+      throw new Error(`Could not get route param: ${paramName}`)
+    }
     return parseInt(param);
   }
 }
