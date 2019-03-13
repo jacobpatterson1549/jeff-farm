8b008edaf1df0b2a4e5b86f4f4289ae99e4ea32a
Jacob Patterson <jacob.patterson@gmail.com>
2019-03-13 13:16:31 -0700

    fixing back [up] button routing

--- jeff-farm-ui/src/app/crud/navigation.component.ts
+++ jeff-farm-ui/src/app/crud/navigation.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router, UrlTree } from '@angular/router';
+import { Location } from '@angular/common';
+import { routerNgProbeToken } from '@angular/router/src/router_module';
 
 @Component({
   selector: 'app-navigation',
@@ -7,15 +9,28 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class NavigationComponent {
 
+  @Input()
+  private stepsToParent?: number = 1;
+
   constructor(
     private router: Router,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute) {
+      if (this.stepsToParent < 1 || !Number.isInteger(this.stepsToParent)) {
+        throw new Error(`Invalid stepsToParent: ${this.stepsToParent}`)
+      }
+    }
 
   canGoUp() {
     return ['/', '/farms'].indexOf(this.router.url) < 0;
   }
 
   goUp() {
-    this.router.navigate(['../'], { relativeTo: this.route } );
+    // if the route has the same path as the parents, we must jump up higher.
+    let parentRoute: ActivatedRoute = this.route;
+    for (var i = 1; i < this.stepsToParent; i++) {
+      parentRoute = parentRoute.parent;
+    }
+
+    this.router.navigate(['..'], { relativeTo:parentRoute } );
   }
 }
