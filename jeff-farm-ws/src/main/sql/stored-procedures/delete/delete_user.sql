80b91d254af672e351d09ad83209d3f4d4547de4
Jacob Patterson <jacob.patterson@gmail.com>
2019-03-18 15:50:03 -0700

    adding stored procs, formatting pom

---
+++ jeff-farm-ws/src/main/sql/stored-procedures/delete/delete_user.sql
@@ -0,0 +1,19 @@
+-- DELIMITER $$
+
+DROP PROCEDURE IF EXISTS delete_user$
+
+CREATE PROCEDURE delete_user (
+	IN id INT)
+
+	BEGIN
+		DELETE ur
+		FROM user_roles AS ur
+		JOIN users AS u ON u.user_name = ur.role_name
+		WHERE u.id = id;
+
+		DELETE u
+		FROM users AS u
+		WHERE u.id = id;
+	END$$
+
+-- DELIMITER ;
