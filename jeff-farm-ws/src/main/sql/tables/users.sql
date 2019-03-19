f6926681cb461471e3c4327357b2ea3200ad7fa1
Jacob Patterson <jacob.patterson@gmail.com>
2019-03-19 10:12:59 -0700

    fixing table

--- jeff-farm-ws/src/main/sql/tables/users.sql
+++ jeff-farm-ws/src/main/sql/tables/users.sql
@@ -6,5 +6,7 @@ CREATE TABLE IF NOT EXISTS users
 	user_name VARCHAR(20) UNIQUE NOT NULL,
 	user_password VARCHAR(20) NOT NULL, -- TODO: this is stored as plaintext (NOT DESIRED)
 	first_name VARCHAR(255),
-	last_name VARCHAR(255)
+	last_name VARCHAR(255),
+	created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+	modified_date DATETIME ON UPDATE CURRENT_TIMESTAMP
 );
