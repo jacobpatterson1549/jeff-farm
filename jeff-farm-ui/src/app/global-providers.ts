01e1dc7284acb6f5176bd2a4a10a7903d200b1fc
Jacob Patterson <jacob.patterson@gmail.com>
2019-04-01 11:18:58 -0700

    removing json interceptor.  it is not needed.

--- jeff-farm-ui/src/app/global-providers.ts
+++ jeff-farm-ui/src/app/global-providers.ts
@@ -1,7 +1,6 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HostInterceptor } from './host-interceptor';
 import { AuthInterceptor } from './auth/auth-interceptor';
-import { JsonInterceptor } from './json-interceptor';
 import { ErrorHandler } from '@angular/core';
 import { ErrorsHandler } from './errors-handler';
 
@@ -9,6 +8,5 @@ import { ErrorsHandler } from './errors-handler';
 export const GlobalProviders = [
     { provide: HTTP_INTERCEPTORS, useClass: HostInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: JsonInterceptor, multi: true },
     { provide: ErrorHandler, useClass: ErrorsHandler },
 ];
