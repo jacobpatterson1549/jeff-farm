dfaac2d618e6931b2f26959c0c0f60a27639839c
Jacob Patterson <jacob.patterson@gmail.com>
2019-04-13 15:22:48 -0700

    adding info on how to add postgresql driver to tomcat

--- jeff-farm-ws/src/main/sql/init.sql
+++ jeff-farm-ws/src/main/sql/init.sql
@@ -15,6 +15,7 @@ CREATE USER farmer with PASSWORD 'flint5+0N3';
 -- wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java_8.0.15-1ubuntu18.04_all.deb
 -- sudo dpkg -i mysql-connector-java_8.0.15-1ubuntu18.04_all.deb || sudo -k
 -- ./asadmin add-library /usr/share/java/mysql-connector-java-8.0.15.jar
+-- cp ~/.m2/repository/org/postgresql/postgresql/42.2.5/postgresql-42.2.5.jar ~/apps/apache-tomcat-9.0.16/lib/
 
 -- SETUP:
 INSERT INTO roles (role_name) VALUES ('user');
