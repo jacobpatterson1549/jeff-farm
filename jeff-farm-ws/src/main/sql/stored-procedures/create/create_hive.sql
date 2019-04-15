2659da4c2a0bfcbf54473ccfd7178bc139341c23
Jacob Patterson <jacob.patterson@gmail.com>
2019-04-15 14:27:05 -0700

    formatting

--- jeff-farm-ws/src/main/sql/stored-procedures/create/create_hive.sql
+++ jeff-farm-ws/src/main/sql/stored-procedures/create/create_hive.sql
@@ -7,16 +7,16 @@ CREATE OR REPLACE FUNCTION create_hive
 AS
 $body$
 	INSERT INTO hives
-			( farm_id
-			, name
-			, queen_color
-			)
-		SELECT
-			  create_hive.farm_id
-			, create_hive.name
-			, create_hive.queen_color
-		FROM farms AS f
-		WHERE f.id = create_hive.farm_id
-		RETURNING id;
+		( farm_id
+		, name
+		, queen_color
+		)
+	SELECT
+		  create_hive.farm_id
+		, create_hive.name
+		, create_hive.queen_color
+	FROM farms AS f
+	WHERE f.id = create_hive.farm_id
+	RETURNING id;
 $body$
 LANGUAGE SQL;
