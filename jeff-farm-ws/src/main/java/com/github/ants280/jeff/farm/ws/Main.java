cb1d6371832858757bffe3f0117876c8249b5d8e
Jacob Patterson <jacob.patterson@gmail.com>
2019-04-18 15:21:15 -0700

    removing extends

--- jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/Main.java
+++ jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/Main.java
@@ -8,7 +8,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.catalina.startup.Tomcat;
 
-public class Main extends Tomcat
+public class Main
 {
 	public static void main(String[] args) throws Exception
 	{
