87952f1fc2cb6f7b36b1cf5f26aaa3e6df720cee
Jacob Patterson <jacob.patterson@gmail.com>
2019-04-22 09:01:49 -0700

    Adding engines to package.json.  Updating how static files are served: https://medium.com/@hellotunmbi/how-to-deploy-angular-application-to-heroku-1d56e09c5147

--- jeff-farm-ui/server.js
+++ jeff-farm-ui/server.js
@@ -2,12 +2,15 @@ var express = require('express');
 
 var app = express();
 
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+// Serve only the static files form the dist directory
+var app_name = 'jeff-farm-ui'
+app.use(express.static(__dirname + '/dist/' + app_name));
+app.get('/*', function(req,res) { 
+    res.sendFile(path.join(__dirname+'/dist' + app_name + '/index.html'));
+});
 
 // Initialize the app.
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
-    console.log("App now running on port", port);
+    console.log('App now running on port', port);
 });
