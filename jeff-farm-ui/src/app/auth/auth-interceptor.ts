8ba85b0e2f79f272c7e5346fe798936af0e5edeb
Jacob Patterson <jacob.patterson@gmail.com>
2019-04-22 11:17:28 -0700

    removing AuthService from AuthInterceptor

--- jeff-farm-ui/src/app/auth/auth-interceptor.ts
+++ jeff-farm-ui/src/app/auth/auth-interceptor.ts
@@ -1,12 +1,11 @@
 import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    constructor(private authService: AuthService) { }
+    constructor() { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req.clone({
