444376738a184abdbd3c32ecbbe1996f151b8cea
Jacob Patterson <jacob.patterson@gmail.com>
2019-04-24 10:08:59 -0700

    bootstrapping error message component

--- jeff-farm-ui/src/app/error-messages/error-messages.component.ts
+++ jeff-farm-ui/src/app/error-messages/error-messages.component.ts
@@ -4,9 +4,14 @@ import { ErrorMessagesService } from './error-messages.service';
 @Component({
   selector: 'error-messages',
   templateUrl: './error-messages.component.html',
-  styleUrls: ['./error-messages.component.css']
 })
 export class ErrorMessagesComponent {
 
   constructor(public errorMessagesService: ErrorMessagesService) { }
+
+  close(message: string) {
+    // TODO: better indexing (indexOf finds first)
+    const index: number = this.errorMessagesService.messages.indexOf(message);
+    this.errorMessagesService.messages.splice(index, 1);
+  }
 }
