daf309273268bdc2c2fa0be37201ddfe371e9182
Jacob Patterson <jacob.patterson@gmail.com>
2019-05-01 09:41:05 -0700

    formatting

--- jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/JeffFarmWsExceptionMapper.java
+++ jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/JeffFarmWsExceptionMapper.java
@@ -5,15 +5,16 @@ import java.util.logging.Logger;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
-public class JeffFarmWsExceptionMapper implements ExceptionMapper<JeffFarmWsException>
+public class JeffFarmWsExceptionMapper
+	implements ExceptionMapper<JeffFarmWsException>
 {
-	@Override
-	public Response toResponse(JeffFarmWsException exception)
+	@Override public Response toResponse(JeffFarmWsException exception)
 	{
 		Logger.getLogger(this.getClass().getName())
 			.log(Level.WARNING, "Caught Exception", exception);
 
 		return Response.status(Response.Status.BAD_REQUEST)
-			.entity(exception.getMessage()).build();
+			.entity(exception.getMessage())
+			.build();
 	}
 }
