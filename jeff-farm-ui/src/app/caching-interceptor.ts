a843bca0c913dfbb36803f14038cad8a8ce2005d
Jacob Patterson <jacob.patterson@gmail.com>
2019-05-03 14:42:29 -0700

    updating caching interceptor to remove the grandparent GET from the cache incase the crud item is the last in the cache.  Ex: {farm x : [hives[y]} - if hive y is deleted, the cache will clear farms/x/hives/y, farms/x/hives (parent), and farms/x (grandparent).

--- jeff-farm-ui/src/app/caching-interceptor.ts
+++ jeff-farm-ui/src/app/caching-interceptor.ts
@@ -30,6 +30,8 @@ export class CachingInterceptor implements HttpInterceptor {
                 this.cachingService.remove(req.url);
                 const parentUrl = req.url.substr(0, req.url.lastIndexOf('/'));
                 this.cachingService.remove(parentUrl);
+                const grandParentUrl = parentUrl.substr(0, parentUrl.lastIndexOf('/'));
+                this.cachingService.remove(grandParentUrl);
 
                 return next.handle(req);
 
