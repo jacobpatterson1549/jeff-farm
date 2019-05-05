470c6d3edd345e11319b77f23e0910ea3c523876
Jacob Patterson <jacob.patterson@gmail.com>
2019-05-05 09:45:32 -0700

    adding more testing/linting base code from angular.  Adding title h1 to page

---
+++ jeff-farm-ui/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { AppPage } from './app.po';
+import { browser, logging } from 'protractor';
+
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getH1Text()).toEqual('Welcome to small!');
+    expect(page.getH1Text()).toEqual('Just Effective Farm Facilitation Login');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
--- jeff-farm-ui/src/app/app.component.ts
+++ jeff-farm-ui/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <h1>Just Effective Farm Facilitation</h1>
     <app-error-messages></app-error-messages>
     <router-outlet></router-outlet>`
 })
