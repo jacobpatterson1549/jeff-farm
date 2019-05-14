708a4b7a3e1424a924b767dc9e7e5754279f4c7b
Jacob Patterson <jacob.patterson@gmail.com>
2019-05-14 14:53:26 -0700

    adding farmId

--- jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/model/Poultry.java
+++ jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/model/Poultry.java
@@ -2,13 +2,25 @@ package com.github.ants280.jeff.farm.ws.model;
 
 public class Poultry extends CrudItem<Poultry>
 {
+	public static final String FARM_ID_COLUMN = "farm_id";
 	public static final String NAME_COLUMN = "name";
+	private int farmId;
 	private String name;
 
 	@Override
 	protected Poultry getThis()
 	{
 		return this;
+	}
+
+	public int getFarmId()
+	{
+		return farmId;
+	}
+
+	public void setFarmId(int farmId)
+	{
+		this.farmId = farmId;
 	}
 
 	public String getName()
