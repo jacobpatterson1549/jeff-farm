cfa4b0812729adf91e0a10420fb46e3bac3172d8
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-07 15:04:29 -0700

    implementing CrudFormEditor enough for the view not to crash.

--- jeff-farm-ui/src/app/crud/crud-item-group-form/crud-item-group-form.component.ts
+++ jeff-farm-ui/src/app/crud/crud-item-group-form/crud-item-group-form.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudFormEditor } from '../crud-form/crud-form-editor';
+import { CrudItemFormComponent } from '../crud-item-form/crud-item-form.component';
+import { CrudItem } from '../crud.item';
+import { CrudItemGroup } from '../crud.item.group';
+import { CrudService } from '../crud.service';
+import { FormType } from '../form.type';
 
 @Component({
   selector: 'app-crud-item-group-form',
   templateUrl: './crud-item-group-form.component.html',
 })
-export class CrudItemGroupFormComponent implements OnInit {
+export class CrudItemGroupFormComponent<U extends CrudItem, V extends CrudItem, T extends CrudItemGroup<V>>
+  extends CrudItemFormComponent<T>
+  implements CrudFormEditor<T>, OnInit {
 
-  constructor() { }
+  constructor(crudService: CrudService<T>) {
+    super(crudService);
+  }
 
   ngOnInit() {
+  }
+
+
+  setFormType(formType: FormType) { }
+
+  isValid(): boolean {
+    return false;
+  }
+
+  getCrudItem(): T {
+    return null;
   }
 }
