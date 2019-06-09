9396f30830d009ff7e1915f6af57272be1f62eea
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-09 13:28:34 -0700

    fixing env

--- jeff-farm-ui/src/environments/environment.prod.ts
+++ jeff-farm-ui/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  // SERVER_URL: 'https://jeff-farm-ws.herokuapp.com/jeff-farm-ws/',
-  SERVER_URL: 'http://localhost:8080/jeff-farm-ws/',
+  SERVER_URL: 'https://jeff-farm-ws.herokuapp.com/jeff-farm-ws/',
   production: true
 };
