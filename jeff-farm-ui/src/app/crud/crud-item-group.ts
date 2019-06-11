84a73f59869e9192a4433b4f9a9b2c0f81074a5e
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-11 15:57:07 -0700

    adding TODO

--- jeff-farm-ui/src/app/crud/crud-item-group.ts
+++ jeff-farm-ui/src/app/crud/crud-item-group.ts
@@ -1,6 +1,7 @@
 import { CrudItem } from './crud-item';
 import { CrudItemInspection } from './crud-item-inspection';
 
+// TODO: rename to CrudItemInspectionGroup
 export abstract class CrudItemGroup<V extends CrudItemInspection<CrudItem>> extends CrudItem {
 
     public inspectionItems: V[] = [];
