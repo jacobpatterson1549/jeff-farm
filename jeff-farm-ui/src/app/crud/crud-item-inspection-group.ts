df4dfe6d88838deba7cd6b5c7f7620604e622a32
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-13 11:15:06 -0700

    adding validator to ensure inspectionItems are non empty (required).  Adding semicolon

--- jeff-farm-ui/src/app/crud/crud-item-inspection-group.ts
+++ jeff-farm-ui/src/app/crud/crud-item-inspection-group.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, FormGroup, AbstractControl, FormArray } from '@angular/forms';
+import { FormBuilder, FormGroup, AbstractControl, FormArray, ValidatorFn, Validators } from '@angular/forms';
 import { CrudItem } from './crud-item';
 import { CrudItemInspection } from './crud-item-inspection';
 
@@ -12,9 +12,9 @@ export abstract class CrudItemInspectionGroup<V extends CrudItemInspection<CrudI
 
     getFormGroup(fb: FormBuilder): FormGroup {
         const inspectionItemGroups: FormGroup[] = this.inspectionItems.map((inspectionItem: V) => inspectionItem.getFormGroup(fb));
-        const array: FormArray = new FormArray(inspectionItemGroups);
+        const array: FormArray = new FormArray(inspectionItemGroups, [Validators.required]);
         const group: FormGroup = super.getFormGroup(fb);
-        group.addControl('inspectionItems', array)
+        group.addControl('inspectionItems', array);
         return group;
     }
 }
