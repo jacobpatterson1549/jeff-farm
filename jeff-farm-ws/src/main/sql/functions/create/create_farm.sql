765d3d7f4f2ef7b117b1bf805aebf6f636970929
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-15 16:01:13 -0700

    adding permission when creating a new farm.

--- jeff-farm-ws/src/main/sql/functions/create/create_farm.sql
+++ jeff-farm-ws/src/main/sql/functions/create/create_farm.sql
@@ -7,14 +7,28 @@ CREATE FUNCTION create_farm
 	)
 AS
 $body$
-	INSERT INTO farms
-		( name
-		, location
-		)
-	SELECT
-		  create_farm.name
-		, create_farm.location
-	WHERE permission_check_user(set_user_id(user_id), user_id)
-	RETURNING id;
+	WITH new_farm AS (
+		INSERT INTO farms
+			( name
+			, location
+			)
+		SELECT
+			  create_farm.name
+			, create_farm.location
+		WHERE permission_check_user(set_user_id(user_id), user_id)
+		RETURNING id
+	)
+	, new_farm_permission AS (
+		INSERT INTO farm_permissions
+			( farm_id
+			, user_id
+			)
+		SELECT
+			 f.id
+			, create_farm.user_id
+		FROM new_farm AS f
+	)
+	SELECT id
+	FROM new_farm;
 $body$
 LANGUAGE SQL;
