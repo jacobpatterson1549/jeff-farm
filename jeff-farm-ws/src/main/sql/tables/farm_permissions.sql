b9ff38295ca107c911020f90f32867d51b48ff28
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-15 16:14:31 -0700

    adding foreign keys to farm_permissions

--- jeff-farm-ws/src/main/sql/tables/farm_permissions.sql
+++ jeff-farm-ws/src/main/sql/tables/farm_permissions.sql
@@ -5,4 +5,9 @@ CREATE TABLE IF NOT EXISTS farm_permissions
 	( farm_id INT NOT NULL
 	, user_id INT NOT NULL
 	, PRIMARY KEY (farm_id, user_id)
+
+	, FOREIGN KEY (farm_id)
+		REFERENCES farms (id)
+	, FOREIGN KEY (user_id)
+		REFERENCES users (id)
 	);
