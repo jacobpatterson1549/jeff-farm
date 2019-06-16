366eab26e305925773e05eff2a2b7a9ae8ee0cd3
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-15 17:08:01 -0700

    simplifying SELECT

--- jeff-farm-ws/src/main/sql/functions/permission_check/permission_check_user.sql
+++ jeff-farm-ws/src/main/sql/functions/permission_check/permission_check_user.sql
@@ -7,7 +7,7 @@ CREATE FUNCTION permission_check_user
 AS
 $body$
 	BEGIN
-		SELECT CASE WHEN user_id <> target_user_id THEN FALSE ELSE TRUE END INTO permission_check;
+		SELECT CASE WHEN user_id = target_user_id THEN TRUE ELSE FALSE END INTO permission_check;
 		IF NOT permission_check THEN
 			RAISE EXCEPTION 'User % does not have access to user %.', user_id, target_user_id;
 		END IF;
