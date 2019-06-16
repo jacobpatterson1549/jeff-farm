fd2e9a377565851fe3cad4f247d521b761804a5d
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-15 18:36:07 -0700

    fixing script and adding it to dbchanges

--- jeff-farm-ws/src/main/sql/functions/permission_check/permission_check_poultry_inspection_group.sql
+++ jeff-farm-ws/src/main/sql/functions/permission_check/permission_check_poultry_inspection_group.sql
@@ -9,8 +9,8 @@ $body$
 	BEGIN
 		SELECT CASE WHEN COUNT(*) = 0 THEN FALSE ELSE TRUE END INTO permission_check
 			FROM farm_permissions AS fp
-			JOIN poultry_inspection_groups AS pig ON h.id = pig.hive_id
-			WHERE fp.user_id = permission_check_hive.user_id
+			JOIN poultry_inspection_groups AS pig ON fp.farm_id = pig.farm_id
+			WHERE fp.user_id = permission_check_poultry_inspection_group.user_id
 				AND pig.id = permission_check_poultry_inspection_group.poultry_inspection_group_id;
 		IF NOT permission_check THEN
 			RAISE EXCEPTION 'User % does not have access to poultry inspection group %.', permission_check_poultry_inspection_group.user_id, permission_check_poultry_inspection_group.poultry_inspection_group_id;
