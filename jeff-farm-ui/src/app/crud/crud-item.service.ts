73f80c5d8ef158da62df8f120c096709cf73019e
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-18 10:40:07 -0700

    formatting

--- jeff-farm-ui/src/app/crud/crud-item.service.ts
+++ jeff-farm-ui/src/app/crud/crud-item.service.ts
@@ -89,8 +89,9 @@ export abstract class CrudItemService<T extends CrudItem> {
   }
 
   protected getRouteParam(paramName: string): string {
-    const param: string = this.route == null ? null : this.route.snapshot.paramMap.get(paramName); // TODO : cean this up
-    return param;
+    return this.route == null
+      ? null
+      : this.route.snapshot.paramMap.get(paramName);
   }
 
   protected getId(): string {
