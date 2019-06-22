b20b514f5422ec0ffb6546b94332a5fd94d94b53
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-22 10:41:06 -0700

    Fixing imports, formatting comment.

--- jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/model/PoultryInspectionGroupUpdate.java
+++ jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/model/PoultryInspectionGroupUpdate.java
@@ -1,9 +1,7 @@
 package com.github.ants280.jeff.farm.ws.model;
 
-import java.util.List;
-
 public class PoultryInspectionGroupUpdate
 	extends CrudItemInspectionGroupUpdate<PoultryInspection, PoultryInspectionGroup>
 {
-	// Class is forfor deserialization of typed CrudItemInspectionGroupUpdate
+	// Class is for deserialization of typed CrudItemInspectionGroupUpdate.
 }
