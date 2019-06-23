96a2a61fb563b8432ebe37261b71b6447ee5d9a2
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-23 10:35:55 -0700

    adding Highcharts

--- jeff-farm-ui/src/app/crud/crud-chart/crud-chart.component.ts
+++ jeff-farm-ui/src/app/crud/crud-chart/crud-chart.component.ts
@@ -1,3 +1,5 @@
+import * as Highcharts from 'highcharts';
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -17,6 +19,43 @@ export class CrudChartComponent
   implements OnInit {
 
   groups: T[];
+  public options: any = {
+    chart: {
+      type: 'scatter',
+      height: 700
+    },
+    title: {
+      text: 'Sample Scatter Plot'
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      formatter() {
+        return `x: ${Highcharts.dateFormat('%e %b %y %H:%M:%S', this.x)} y: ${this.y.toFixed(2)}`;
+      }
+    },
+    xAxis: {
+      type: 'datetime',
+      labels: {
+        formatter() {
+          return Highcharts.dateFormat('%e %b %y', this.value);
+        }
+      }
+    },
+    series: [
+      {
+        name: 'Normal',
+        turboThreshold: 500000,
+        data: [[new Date('2018-01-25 18:38:31').getTime(), 2]]
+      },
+      {
+        name: 'Abnormal',
+        turboThreshold: 500000,
+        data: [[new Date('2018-02-05 18:38:31').getTime(), 7]]
+      }
+    ]
+  };
 
   constructor(
     route: ActivatedRoute,
@@ -25,6 +64,7 @@ export class CrudChartComponent
   }
 
   ngOnInit() {
+    Highcharts.chart('chart', this.options);
     this.crudItemInspectionGroupService.getList()
       .subscribe((groups: T[]) => {
         this.groups = groups;
