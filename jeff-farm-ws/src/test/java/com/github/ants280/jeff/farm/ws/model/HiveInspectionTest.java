200eebd63ba6f586e77ae6bfa2f690bf4dabd005
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-22 17:52:17 -0700

    100% coverage :p

--- jeff-farm-ws/src/test/java/com/github/ants280/jeff/farm/ws/model/HiveInspectionTest.java
+++ jeff-farm-ws/src/test/java/com/github/ants280/jeff/farm/ws/model/HiveInspectionTest.java
@@ -18,9 +18,34 @@ public class HiveInspectionTest
 	}
 
 	@Test
+	public void testDeserialize_basic()
+	{
+		String serializedHiveInspection = "{\"id\":47,\"parentId\":19,\"queenSeen\":false,\"eggsSeen\":true,\"layingPatternStars\":3,\"temperamentStars\":4,\"queenCells\":11,\"supersedureCells\":22,\"swarmCells\":32,\"combBuildingStars\":5,\"framesSealedBrood\":14,\"framesOpenBrood\":8,\"framesHoney\":6,\"weather\":\"hott\",\"temperatureF\":83,\"windSpeedMph\":17}";
+
+		HiveInspection hiveInspection = jsonb.fromJson(serializedHiveInspection, HiveInspection.class);
+
+		assertThat(hiveInspection.getId(), is(47));
+		assertThat(hiveInspection.getParentId(), is(19));
+		assertThat(hiveInspection.getQueenSeen(), is(false));
+		assertThat(hiveInspection.getEggsSeen(), is(true));
+		assertThat(hiveInspection.getLayingPatternStars(), is(3));
+		assertThat(hiveInspection.getTemperamentStars(), is(4));
+		assertThat(hiveInspection.getQueenCells(), is(11));
+		assertThat(hiveInspection.getSupersedureCells(), is(22));
+		assertThat(hiveInspection.getSwarmCells(), is(32));
+		assertThat(hiveInspection.getCombBuildingStars(), is(5));
+		assertThat(hiveInspection.getFramesSealedBrood(), is(14));
+		assertThat(hiveInspection.getFramesOpenBrood(), is(8));
+		assertThat(hiveInspection.getFramesHoney(), is(6));
+		assertThat(hiveInspection.getWeather(), is("hott"));
+		assertThat(hiveInspection.getTemperatureF(), is(83));
+		assertThat(hiveInspection.getWindSpeedMph(), is(17));
+	}
+
+	@Test
 	public void testDeserialize_nullBooleanShouldBeFalse()
 	{
-		String serializedHiveInspection = "{\"hiveId\":47,\"queenSeen\":true,\"eggsSeen\":null,\"layingPatternStars\":3,\"temperamentStars\":4,\"queenCells\":1,\"supersedureCells\":2,\"swarmCells\":3,\"combBuildingStars\":2,\"framesSealedBrood\":0,\"framesOpenBrood\":8,\"framesHoney\":6,\"weather\":\"hott\",\"temperatureF\":83,\"windSpeedMph\":6,\"id\":null}";
+		String serializedHiveInspection = "{\"queenSeen\":true,\"eggsSeen\":null}";
 
 		HiveInspection hiveInspection = jsonb.fromJson(serializedHiveInspection, HiveInspection.class);
 
