3a6b1829e3126dc0a41ee4a781ec6ae8cc489f6a
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-22 19:57:22 -0700

    Allowing notes columns to be null (like the ui currently does.

--- jeff-farm-ws/src/main/sql/adhoc_after.sql
+++ jeff-farm-ws/src/main/sql/adhoc_after.sql
@@ -6,5 +6,8 @@ BEGIN
 	ALTER TABLE poultry_inspections_audit ALTER COLUMN bird_count DROP NOT NULL;
 	ALTER TABLE poultry_inspections_audit ALTER COLUMN egg_count  DROP NOT NULL;
 
+	ALTER TABLE hive_inspection_groups    ALTER COLUMN notes DROP NOT NULL;
+	ALTER TABLE poultry_inspection_groups ALTER COLUMN notes DROP NOT NULL;
+
 END
 $$;
