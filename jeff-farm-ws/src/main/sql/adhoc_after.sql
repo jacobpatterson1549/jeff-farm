b0863d2d2726a925b659dfd0ba599b708443a939
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-22 19:55:09 -0700

    Allowing nullable values in poultry_inspections_audit, similar to other tables.

--- jeff-farm-ws/src/main/sql/adhoc_after.sql
+++ jeff-farm-ws/src/main/sql/adhoc_after.sql
@@ -1,5 +1,10 @@
 DO $$
 BEGIN
 
+	ALTER TABLE poultry_inspections_audit ALTER COLUMN group_id   DROP NOT NULL;
+	ALTER TABLE poultry_inspections_audit ALTER COLUMN target_id  DROP NOT NULL;
+	ALTER TABLE poultry_inspections_audit ALTER COLUMN bird_count DROP NOT NULL;
+	ALTER TABLE poultry_inspections_audit ALTER COLUMN egg_count  DROP NOT NULL;
+
 END
 $$;
