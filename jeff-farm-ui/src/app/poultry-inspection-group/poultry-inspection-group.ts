81f1b791a1246bab33b0e3bba738c198867c3fe1
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-22 17:28:52 -0700

    imports

--- jeff-farm-ui/src/app/poultry-inspection-group/poultry-inspection-group.ts
+++ jeff-farm-ui/src/app/poultry-inspection-group/poultry-inspection-group.ts
@@ -1,4 +1,3 @@
-import { CrudItem } from '../crud/crud-item';
 import { CrudItemInspectionGroup } from '../crud/crud-item-inspection-group';
 import { FormItem, FormItemType } from '../crud/form-item';
 import { PoultryInspection } from './poultry-inspection';
