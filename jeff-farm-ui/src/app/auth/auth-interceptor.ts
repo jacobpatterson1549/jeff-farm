89d401dd65c6b60b2c47f390572f58ff1da7a083
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-24 12:26:22 -0700

    using jsessionId in the url for session tracking

--- jeff-farm-ui/src/app/auth/auth-interceptor.ts
+++ jeff-farm-ui/src/app/auth/auth-interceptor.ts
@@ -3,13 +3,16 @@ import { Observable } from 'rxjs';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { AuthService } from './auth.service';
+
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    constructor(private authService: AuthService) { }
 
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req.clone({
-            withCredentials: true
+            url: `${req.url};jsessionid=${this.authService.getJsessionId()}`,
         }));
     }
 }
--- jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/model/LoginSuccess.java
+++ jeff-farm-ws/src/main/java/com/github/ants280/jeff/farm/ws/model/LoginSuccess.java
@@ -4,11 +4,13 @@ public class LoginSuccess
 {
 	private final boolean adminUser;
 	private final int userId;
+	private final String jsessionId;
 
-	public LoginSuccess(boolean adminUser, int userId)
+	public LoginSuccess(boolean adminUser, int userId, String jsessionId)
 	{
 		this.adminUser = adminUser;
 		this.userId = userId;
+		this.jsessionId = jsessionId;
 	}
 
 	public boolean getAdminUser()
@@ -19,5 +21,10 @@ public class LoginSuccess
 	public int getUserId()
 	{
 		return userId;
+	}
+
+	public String getJsessionId()
+	{
+		return jsessionId;
 	}
 }
