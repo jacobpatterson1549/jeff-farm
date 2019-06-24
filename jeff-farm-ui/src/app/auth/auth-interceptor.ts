7eb137bf7066ebecdc0e74814949fd911df30d88
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-24 11:52:50 -0700

    removing unused service.

--- jeff-farm-ui/src/app/auth/auth-interceptor.ts
+++ jeff-farm-ui/src/app/auth/auth-interceptor.ts
@@ -3,12 +3,8 @@ import { Observable } from 'rxjs';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { AuthService } from './auth.service';
-
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
-    constructor(private authService: AuthService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
