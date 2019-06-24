3e75008b71c65f2ac10fb72ec0a7dec73ec84d56
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-23 18:00:39 -0700

    adding selector to allow ng-component element to work

--- jeff-farm-ui/src/app/page-not-found.component.ts
+++ jeff-farm-ui/src/app/page-not-found.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'app-page-not-found',
   template: '<ngb-alert [dismissible]="false" type="danger">Page not found</ngb-alert>',
 })
 export class PageNotFoundComponent { }
