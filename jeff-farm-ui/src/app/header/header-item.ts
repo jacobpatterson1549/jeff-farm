cd1fa3e9f6a76499ceda910b6f9531f7387f0b86
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-25 11:15:30 -0700

    adding headerItem backend

--- jeff-farm-ui/src/app/header/header-item.ts
+++ jeff-farm-ui/src/app/header/header-item.ts
@@ -1,4 +1,38 @@
-export interface HeaderItem {
-    name: string;
+export enum HeaderItemType {
+    FARM,
+    HIVE,
+    HIVE_INSPECTION_GROUP,
+    POULTRY,
+    POULTRY_INSPECTION_GROUP,
+    USER,
+}
+
+export class HeaderItem {
     url: string;
+    type: HeaderItemType;
+    id: number;
+    name: string;
+
+    static from(url: string, name: string): HeaderItem {
+        return new HeaderItem(url, null, null, name);
+    }
+
+    // constructor(url: string, name: string);
+    constructor(url: string, type: HeaderItemType, id: number, name?: string) {
+        this.url = url;
+        this.type = type;
+        this.id = id;
+        this.name = name;
+    }
+
+    getTypeName(): string {
+        switch (this.type) {
+            case HeaderItemType.FARM: return 'Farm';
+            case HeaderItemType.HIVE: return 'Hive';
+            case HeaderItemType.HIVE: return 'Poultry';
+            case HeaderItemType.HIVE_INSPECTION_GROUP:
+            case HeaderItemType.POULTRY_INSPECTION_GROUP: return 'inspection';
+            default: return 'unknown';
+        }
+    }
 }
--- jeff-farm-ui/src/app/header/header.service.ts
+++ jeff-farm-ui/src/app/header/header.service.ts
@@ -1,24 +1,44 @@
 import { Injectable } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
-import { HeaderItem } from './header-item';
+import { HeaderItem, HeaderItemType } from './header-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeaderService {
 
-  // headerItems: HeaderItem[] = [];
-  headerItems: HeaderItem[] = [
-    { name: 'a', url: 'aa' },
-    { name: 'b', url: 'bb' },
-    { name: 'c', url: 'cc' },
-  ];
+  headerItems: HeaderItem[] = [];
+  cache: object = {}; // HeaderItemType:id:name
 
-  constructor(private titleService: Title) { }
+  constructor(private titleService: Title) {
+    for (const type in HeaderItemType) {
+      if (type !== null) {
+        this.cache[type] = {};
+      }
+    }
+
+    // DEBUG setup:
+    this.setPage('xyz', [
+      HeaderItem.from('aa', 'a'),
+      HeaderItem.from('bb', 'b'),
+      HeaderItem.from('cc', 'c'),
+      new HeaderItem('/farm/14', HeaderItemType.FARM, 14),
+    ]);
+  }
 
   setPage(title: string, headerItems: HeaderItem[]) {
     this.titleService.setTitle(title);
     this.headerItems = headerItems;
+
+    for (const headerItem of this.headerItems) {
+      if (headerItem.type !== null && headerItem.id !== null) {
+        this.cache[headerItem.type][headerItem.id] = headerItem.name;
+      }
+      if (!headerItem.name) {
+        headerItem.name = this.cache[headerItem.type][headerItem.id]
+          || `[${headerItem.getTypeName()} #${headerItem.id}]`;
+      }
+    }
   }
 }
