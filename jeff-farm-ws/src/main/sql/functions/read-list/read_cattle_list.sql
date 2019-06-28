27ec92a4abdce61dcb2ec0bec7f64be1be476374
Jacob Patterson <jacob.patterson@gmail.com>
2019-06-27 19:13:14 -0700

    fixing cattle function

--- jeff-farm-ws/src/main/sql/functions/read-list/read_cattle_list.sql
+++ jeff-farm-ws/src/main/sql/functions/read-list/read_cattle_list.sql
@@ -17,7 +17,7 @@ $body$
 				, c.modified_date
 			FROM cattle AS c
 			WHERE c.farm_id = read_cattle_list.farm_id
-			ORDER BY h.created_date DESC;
+			ORDER BY c.created_date DESC;
 		END IF;
 	END
 $body$
