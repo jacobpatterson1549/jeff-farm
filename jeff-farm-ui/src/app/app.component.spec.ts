358db737165098560fafefd214603c14e2a3eabf
Jacob Patterson <jacob.patterson@gmail.com>
2019-07-02 10:35:31 -0700

    Fixing tests by adding imports.

--- jeff-farm-ui/src/app/app.component.spec.ts
+++ jeff-farm-ui/src/app/app.component.spec.ts
@@ -3,6 +3,8 @@ import { async, TestBed } from '@angular/core/testing';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorMessagesModule } from './error-messages/error-messages.module';
+import { FooterModule } from './footer/footer.module';
+import { HeaderComponent } from './header/header.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 describe('AppComponent', () => {
@@ -11,10 +13,12 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent,
         PageNotFoundComponent,
+        HeaderComponent,
       ],
       imports: [
         AppRoutingModule,
         ErrorMessagesModule,
+        FooterModule,
       ],
     }).compileComponents();
   }));
