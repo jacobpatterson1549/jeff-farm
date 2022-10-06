2fd88d16da88905c64b01e402fd430fa5dd296d4
jacobpatterson1549 <jacob.patterson@gmail.com>
2022-10-06 12:00:17 -0700

    adding psql db creation/restore snippets

--- jeff-farm-ws/src/main/sql/init.sql
+++ jeff-farm-ws/src/main/sql/init.sql
@@ -16,3 +16,20 @@ CREATE USER farmer with PASSWORD 'flint5+0N3';
 -- sudo dpkg -i mysql-connector-java_8.0.15-1ubuntu18.04_all.deb || sudo -k
 -- ./asadmin add-library /usr/share/java/mysql-connector-java-8.0.15.jar
 -- cp ~/.m2/repository/org/postgresql/postgresql/42.2.5/postgresql-42.2.5.jar ~/apps/apache-tomcat-9.0.16/lib/
+
+-- # create postgres user with the following bash command: 
+/*
+PGDATABASE="jeff_farm_db" \
+PGUSER="farmer" \
+PGPASSWORD="flint5+0N3" \
+PGHOSTADDR="127.0.0.1" \
+PGPORT="5432" \
+sh -c ' \
+sudo -u postgres psql \
+-c "CREATE DATABASE $PGDATABASE" \
+-c "CREATE USER $PGUSER WITH ENCRYPTED PASSWORD '"'"'$PGPASSWORD'"'"'" \
+-c "GRANT ALL PRIVILEGES ON DATABASE $PGDATABASE TO $PGUSER" \
+&& echo DATABASE_URL=postgres://$PGUSER:$PGPASSWORD@$PGHOSTADDR:$PGPORT/$PGDATABASE'
+*/
+
+-- pg_restore -d '$DATABASE_URL' file.dump
