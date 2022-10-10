aa27f030e997f49a5d338c19b307ed64e8ddacd2
jacobpatterson1549 <jacob.patterson@gmail.com>
2022-10-10 15:00:29 -0700

    SONAR: using keyword const instead of var

--- jeff-farm-ui/server.js
+++ jeff-farm-ui/server.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 // Serve only the static files form the dist directory
-var app_name = 'jeff-farm-ui'
+const app_name = 'jeff-farm-ui'
 app.use(express.static(path.join(__dirname, 'dist', app_name)));
 app.get('/*', function (_req, res) {
     res.sendFile(path.join(__dirname, 'dist', app_name, 'index.html'));
 });
 
 // Initialize the app.
-var server = app.listen(process.env.PORT || 4200, function () {
-    var port = server.address().port;
+const server = app.listen(process.env.PORT || 4200, function () {
+    const port = server.address().port;
     console.log('App now running on port', port);
 });
 
